ff95b47501a79ede3e8063d3886b7839f8745441
eidorii <formanickisadrians@gmail.com>
2026-04-21 16:08:08 +0200

    feature: add Routine class

--- lib/day_entry.dart
+++ lib/day_entry.dart
@@ -1,3 +1,11 @@
+import 'dart:ui';
+
+class Routine {
+  final String name;
+  final Color color;
+  const Routine({required this.name, required this.color});
+}
+
 class DayEntry {
   final DateTime date;
   final List<String> routines;
